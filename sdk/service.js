4ca948bd84162e23eacd1f3a8e590d0391da6cad
Orestes Carracedo <orestes@orestes.io>
2017-05-31 01:42:29 +0200

    refactor(service): Skip logging error message as it's already part of the stack

--- sdk/service.js
+++ sdk/service.js
@@ -33,7 +33,7 @@ class Service extends Component {
       // User
       this._runError(e);
       // Logs
-      logger.error(`Error running callback: ${actionName}. ${e.message}`, e.stack);
+      logger.error(`Service error running callback "${actionName}"`, e.stack);
       // Transport / Command reply
       this._replyWithError(
         `Middleware error running callback: ${actionName}. ${e.message}`,
