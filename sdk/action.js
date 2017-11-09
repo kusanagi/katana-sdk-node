e9dad377ec0820191a07d862cabcbb11f965fdf2
nullproxy <nullproxy@elitemail.org>
2017-11-09 20:32:52 +0100

    Generate a Param object even if it does not exist

--- sdk/action.js
+++ sdk/action.js
@@ -712,6 +712,9 @@ for action: "${this._actionName}"`
     if (param instanceof Param) {
       return param;
     }
+    if (!param) {
+      return new Param(name, '', 'string', false);
+    }
     return new Param(name, param[m.value], param[m.type], this.hasParam(name));
   }
 
