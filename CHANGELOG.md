848c61ca63752a4a93ed097801af711806049b34
Fernando Castillo <contact@fcastillo.es>
2018-03-01 20:16:24 +0100

    Update copyright

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,15 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
+## [Unreleased]
+
+## [2.0.0] - 2018-03-01
+## Added
+- Support for transport API
+
+## Changed
+- Move primary key to the schema entity
+
 ## [1.2.3] - 2017-09-07
 ## Fixed
 - Fix error in response.setHeader when no other headers are present
--- sdk/action-schema.test.js
+++ sdk/action-schema.test.js
@@ -1,3 +1,17 @@
+/*
+ * NODE SDK for the KATANA(tm) Framework (http://katana.kusanagi.io)
+ * Copyright (c) 2016-2018 KUSANAGI S.L. All rights reserved.
+ *
+ * Distributed under the MIT license
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code
+ *
+ * @link      https://github.com/kusanagi/katana-sdk-node
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ * @copyright Copyright (c) 2016-2018 KUSANAGI S.L. (http://kusanagi.io)
+ */
+
 'use strict';
 
 const ActionSchema = require('./action-schema');
